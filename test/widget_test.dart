0637a95ebc524b91824ab2d43bf77f1cb02e7b40
fadillaoegi <fadilfdl2710@gmail.com>
2024-04-25 13:53:59 +0700

    save

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,27 +1,16 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-
-// import 'package:dilalquran/main.dart';
-
 // ignore_for_file: avoid_print
 
 import 'package:dilalquran/modules/data/models/surah_model.dart';
 import 'package:dilalquran/modules/data/sources/home_source.dart';
 
 void main() async {
-  // print("runTest");
-
-  print(HomeSource.fetchSurah());
-
-  List<Surah> surah = await HomeSource.fetchSurah();
-  print(surah.length);
-
-  // Surah surahh =
+  List<Surah>? surah = await HomeSource.fetchSurah();
+  if (surah.isNotEmpty) {
+    print("Jumlah surah: ${surah.length}");
+    for (var i = 0; i < surah.length; i++) {
+      print("Surah ${i + 1} - Jumlah ayat: ${surah[i].numberOfVerses}");
+    }
+  } else {
+    print("Gagal mendapatkan daftar surah.");
+  }
 }
